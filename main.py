03774c49118310d37a4b6ba223537cfa8722062d
rahulrameshm0 <iamrahulm23@gmail.com>
2025-02-07 21:13:54 +0530

    modified login page and signup page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for, get_flashed_messages
 datas = {}
 
 app = Flask(__name__)
@@ -11,30 +11,45 @@ def home():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        name = None
-        datas[request.form["fname"]] = {
-        "lname": request.form["lname"],
-        "email": request.form["email"],
-        "password": request.form["password"],
-        "confirm": request.form["confirm"]
-        }
+        fname = request.form["fname"]
+        lname = request.form["lname"]
+        email = request.form["email"]
+        username = request.form["username"]
         password = request.form["password"]
         confirm = request.form["confirm"]
+        for user_data in datas.values():
+            if user_data["username"] == username:
+                flash("This is username is already taken!")
+                return redirect(url_for("signup"))
+            
         if password != confirm:
-            name = "Your password does not match"
-            return render_template("signup.html", name = name)
-      
-    return render_template("signup.html", success = "Account Created Successfully!")
+            flash("password does not match!","error")
+            return redirect(url_for("signup"))
+        
+        datas[fname] = {
+            "lname":lname,
+            "email":email,
+            "confirm":confirm,
+            "password":password,
+            "username":username
+        }
+        flash("Account created successfully!!", "success")
+        return redirect(url_for("signup"))
+    return render_template("signup.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        error = None
-        fname = request.form['fname']
-        password = request.form['password']
-        if fname not in datas or datas[fname]["password"] != password:
-            error = "Wrong username or password"
-            return render_template('login.html', error = error)
+        username = request.form.get("username")
+        password = request.form.get("password")
+        for user in datas.values():
+            if user["username"] == username and user["password"] == password:
+                flash("login successful", "success")
+                return redirect(url_for('home'))
+         
+        flash("username or password is incorrect!", "error")
+        return redirect(url_for("login"))
+    
     return render_template("login.html")
 
 if __name__ == "__main__":
